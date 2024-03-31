2bca5fe6c184ec4e2cd6ff86fd46bbe5510a09b8
RikoAppDev <riko.appdev@gmail.com>
2024-03-31 23:48:46 +0200

    active event

--- src/endpoints/events/active.ts
+++ src/endpoints/events/active.ts
@@ -1,33 +1,67 @@
-import { PrismaClient } from "@prisma/client";
+import {
+    AccountType,
+    EventPresenceStatus,
+    EventStatus,
+    PrismaClient
+} from "@prisma/client";
 import { Request, Response } from "express";
 import { ThrowNotFound } from "../../errorResponses/notFound404";
 import { ThrowInternalServerError } from "../../errorResponses/internalServer500";
+import { UserDecodedData } from "../../../@types/jwtToken";
 
 const prisma = new PrismaClient();
 
 export const getActiveEvent = async (req: Request, res: Response) => {
-    const { eventId } = req.params;
+    const userData = req.user as UserDecodedData;
 
     try {
-        const event = await prisma.event.findUnique({
-            where: {
-                id: eventId
-            },
-            include: {
-                Location: true,
+        const myActiveEvent = await prisma.event.findFirst({
+            select: {
+                id: true,
+                thumbnailURL: true,
+                name: true,
                 User: {
                     select: {
                         name: true
                     }
                 }
+            },
+            where: {
+                ...(userData.role === AccountType.HARVESTER
+                    ? {
+                          AND: [
+                              {
+                                  EventAssignment: {
+                                      some: {
+                                          userId: userData.id,
+                                          presenceStatus:
+                                              EventPresenceStatus.PRESENT
+                                      }
+                                  }
+                              },
+                              {
+                                  status: EventStatus.PROGRESS
+                              }
+                          ]
+                      }
+                    : {
+                          AND: [
+                              {
+                                  userId: userData.id
+                              },
+                              {
+                                  status: EventStatus.PROGRESS
+                              }
+                          ]
+                      })
             }
         });
 
-        if (!event) {
+        if (!myActiveEvent) {
             return ThrowNotFound(res);
         }
 
-        return res.status(200).json(event);
+        return res.status(200).json(myActiveEvent);
     } catch (error) {
         return ThrowInternalServerError(res);
     }
