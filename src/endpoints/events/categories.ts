c29a3f5def8c5ac2dd0b49cb3c9d55ea45ad9222
martinvanco <vancoweb.design@gmail.com>
2024-04-07 17:23:35 +0200

    removed comment

--- src/endpoints/events/categories.ts
+++ src/endpoints/events/categories.ts
@@ -6,24 +6,6 @@ const prisma = new PrismaClient();
 
 export const getAssignedCategories = async (req: Request, res: Response) => {
     try {
-        // const categories = await prisma.eventCategoryRelation.findMany({
-        //     where: {
-        //         Event: {
-        //             status: EventStatus.CREATED
-        //         }
-        //     },
-        //     include: {
-        //         EventCategory: {
-        //             include: {
-        //                 _count: {
-        //                     select: {
-        //                         EventCategoryRelation: true
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     }
-        // });
         const categories = await prisma.eventCategory.findMany({
             where: {
                 EventCategoryRelation: {
